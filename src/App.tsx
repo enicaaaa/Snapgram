e6f8f7433f7db87d1beaface888a0d37d62a6c19
Ena Dodić <enicaaaa99@gmail.com>
2024-02-28 21:08:13 +0100

    Initial commit, Signup page

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Routes, Route } from 'react-router-dom';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import Home from './_root/pages/Home';
+import './globals.css'
+
+const App = () => {
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* private */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
+                {/* public */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home/>} />
+                </Route>
+            </Routes>
+        </main>
+    )
+}
+
+export default App;
